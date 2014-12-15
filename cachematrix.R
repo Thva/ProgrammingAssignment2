87be8dcdf4dd2460f110bb15d533550495fe73fa
Thva <th.vanel@gmail.com>
2014-12-15 16:52:29 +0100

    Commit of my modified file cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -12,15 +12,15 @@
 
 makeCacheMatrix <- function(x = matrix())    ## makeCacheMatrix can be called without any argument. If so, one can "initialize" the matrix with the method set()
 {
-    inverse <- NULL    ## 'attribute of 'makeCacheMatrix'
+    inverse <- NULL    ## "attribute" of 'makeCacheMatrix'
     set <- function(y)    ## initialization (needed if argument 'matrix' is not given)
     {
-        x <<- y    ## 'attribute of 'makeCacheMatrix'
+        x <<- y    ## "attribute" of 'makeCacheMatrix'
         inverse <<- NULL
     }
-    get <- function() x    ## returns the matrix attribute
-    setInverse <- function(inv) inverse <<- inv    ## method that sets its argument as the inverse matrix attribute
-    getInverse <- function() inverse    ## method that returns the inverse matrix attribute
+    get <- function() x    ## returns the matrix "attribute"
+    setInverse <- function(inv) inverse <<- inv    ## method that sets its argument as the inverse matrix "attribute"
+    getInverse <- function() inverse    ## method that returns the inverse matrix "attribute"
     list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)  ## Returns a list of functions : one accesses to the functions through list selection
 }
 
@@ -33,14 +33,14 @@ makeCacheMatrix <- function(x = matrix())    ## makeCacheMatrix can be called wi
 
 cacheSolve <- function(x, ...)
 {
-    inverse <- x$getInverse()
-    if(!is.null(inverse))
+    inverse <- x$getInverse()    ## gets the inverse matrix
+    if(!is.null(inverse))    ## if the inverse matrix is already cached
     {
         message("getting cache data")
         return(inverse)
     }
-    matrix <- x$get()    ## get the data from the object x
+    matrix <- x$get()    ## if not, get the data from the object x
     inverse <- solve(matrix, ...)    ## compute the inverse (with possible extra argumments '...')
-    x$setInverse(inverse)    ## set the value of 'inverse' in the inverse matrix attribute of object x
+    x$setInverse(inverse)    ## set the value of 'inverse' in the inverse matrix "attribute" of object x
     inverse    ## returns the inverse matrix
 }
